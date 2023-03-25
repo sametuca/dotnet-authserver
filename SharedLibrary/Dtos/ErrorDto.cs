6b3303191d8963323ce1c628d7c715f59a053dfe
Samet UCA <sametuca@hotmail.com>
2023-03-25 17:53:14 +0300

    generic repository added

---
+++ SharedLibrary/Dtos/ErrorDto.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace SharedLibrary.Dtos
+{
+    public class ErrorDto
+    {
+        public List<string> Errors { get; }
+        public bool IsShow { get; set; }
+
+        public ErrorDto()
+        {
+            Errors = new List<string>();
+        }
+
+        public ErrorDto(string error,bool isShow)
+        {
+            Errors.Add(error);
+            IsShow = isShow;
+        }
+
+        public ErrorDto(List<string> errors, bool isShow)
+        {
+            Errors = errors;
+            IsShow = isShow;
+        }
+    }
+
+}
---
+++ SharedLibrary/Dtos/Response.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharedLibrary.Dtos
+{
+    public class Response<T> where T:class
+    {
+        public T Data { get; set; }
+        public int StatusCode { get; set; }
+
+        public ErrorDto Error { get; set; }
+
+        public static Response<T> Success(T data, int statusCode)
+        {
+            return new Response<T> { Data = data, StatusCode = statusCode };
+        }
+    }
+}
