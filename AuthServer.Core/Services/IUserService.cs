b6a3e068c46dd9874cbfaf24b54d4ecf0a042a1c
Samet UCA <sametuca@hotmail.com>
2023-03-25 18:48:29 +0300

    dbContext added

--- AuthServer.Core/Services/IUserService.cs
+++ AuthServer.Core/Services/IUserService.cs
@@ -1,7 +1,12 @@
-﻿namespace AuthServer.Core.Services
+﻿using System.Threading.Tasks;
+using AuthServer.Core.DTOs;
+using SharedLibrary.Dtos;
+
+namespace AuthServer.Core.Services
 {
     public interface IUserService
     {
-
+        Task<Response<UserAppDto>> CreateUserAsync(CreateUserDto createUserDto);
+        Task<Response<UserAppDto>> GetUserByNameAsync(string name);
     }
 }
