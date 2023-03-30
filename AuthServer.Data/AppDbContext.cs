fb8f2d701a3e0f235cef188a2a5f9b7cc17cce7e
sametuca <samet.uca@marti.tech>
2023-03-30 13:36:00 +0300

    app db context

--- AuthServer.Data/AppDbContext.cs
+++ AuthServer.Data/AppDbContext.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using AuthServer.Core.Models;
+﻿using AuthServer.Core.Models;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -24,6 +19,13 @@ namespace AuthServer.Data
         {
             builder.ApplyConfigurationsFromAssembly(GetType().Assembly);
             base.OnModelCreating(builder);
+            /*
+             * Yukarıdaki kod bloğu, DbContext sınıfının ModelBuilder nesnesine uygulama yapılandırmalarını ekleyen iki satırdan oluşur.
+             * ApplyConfigurationsFromAssembly() yöntemi,
+             * belirtilen derlemedeki tüm konfigürasyon sınıflarını (modeli yapılandırmak için kullanılan sınıflar) tespit eder ve bunları ModelBuilder'a ekler.
+             * Bu yöntem, Fluent API ayarlarını belirlemek için kullanılan ayrı sınıfların oluşturulmasını sağlar ve veritabanı modeli oluşturulurken kullanılacak olan kuralları ve kısıtlamaları tanımlar.
+             * Böylece, veritabanına erişim işlemleri sırasında bu yapılandırmalar etkin hale gelir ve veritabanı işlemleri bu yapılandırmalarla uyumlu olarak gerçekleştirilir.
+             */
         }
     }
 }
